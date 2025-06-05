a9f197b5d29967acd24313370ede19a5a8e529b4
SwairIT <1q2w3e4r5t6y7u89o0pi@gmail.com>
2025-06-05 08:05:43 +0300

    - Initial commit
    add database

--- src/pizzeria_bot/app.py
+++ src/pizzeria_bot/app.py
@@ -4,14 +4,18 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-from aiogram.fsm.strategy import FSMStrategy
+
+from middliwares.db import DataBaseSession
 
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
+from database.engine import create_db, drop_db, session_maker
+
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from handlers.admin_private import admin_router
+
 from common.bot_cmds_list import private
 
 ALLOWED_UPDATES = ['message, edited_message']
@@ -27,7 +31,26 @@ dp = Dispatcher()
 dp.include_routers(user_private_router, user_group_router, admin_router)
 
 
+async def on_startup(bot):
+
+    run_param = False
+    if run_param:
+        await drop_db()
+
+    await create_db()
+
+
+async def on_shutdown(bot):
+    print('бот выключился')
+
+
+
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
+
     await bot.delete_webhook(drop_pending_updates=True)
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
--- src/pizzeria_bot/handlers/admin_private.py
+++ src/pizzeria_bot/handlers/admin_private.py
@@ -3,8 +3,13 @@ from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from filters.chat_types import ChatTypeFilter, IsAdmin
+
 from kbds.reply import get_keyboard
+
+from database.orm_query import orm_add_product
 
 
 admin_router = Router()
@@ -57,7 +62,7 @@ class AddProduct(StatesGroup):
 
 
 @admin_router.message(StateFilter(None), F.text == "Добавить товар")
-async def add_product(message: types.Message, state= FSMContext):
+async def add_product(message: types.Message, state: FSMContext):
     await message.answer(
         "Введите название товара", reply_markup=types.ReplyKeyboardRemove()
     )
@@ -66,7 +71,7 @@ async def add_product(message: types.Message, state= FSMContext):
 
 @admin_router.message(StateFilter('*'), Command("отмена"))
 @admin_router.message(StateFilter('*'), F.text.casefold() == "отмена")
-async def cancel_handler(message: types.Message, state= FSMContext) -> None:
+async def cancel_handler(message: types.Message, state: FSMContext) -> None:
 
     current_state = await state.get_state()
     if current_state is None:
@@ -78,7 +83,7 @@ async def cancel_handler(message: types.Message, state= FSMContext) -> None:
 
 @admin_router.message(StateFilter('*'), Command("назад"))
 @admin_router.message(StateFilter('*'), F.text.casefold() == "назад")
-async def cancel_handler(message: types.Message, state= FSMContext) -> None:
+async def cancel_handler(message: types.Message, state: FSMContext) -> None:
 
     current_state = await state.get_state()
 
@@ -96,49 +101,55 @@ async def cancel_handler(message: types.Message, state= FSMContext) -> None:
 
 
 @admin_router.message(AddProduct.name, F.text)
-async def add_name(message: types.Message, state= FSMContext):
+async def add_name(message: types.Message, state: FSMContext):
     await state.update_data(name=message.text)
     await message.answer("Введите описание товара")
     await state.set_state(AddProduct.description)
 
 
 @admin_router.message(AddProduct.name)
-async def add_name(message: types.Message, state= FSMContext):
+async def add_name(message: types.Message, state: FSMContext):
     await message.answer("Вы ввели недопустимые данные, введите текст названия товара")
 
 
 @admin_router.message(AddProduct.description, F.text)
-async def add_description(message: types.Message, state= FSMContext):
+async def add_description(message: types.Message, state: FSMContext):
     await state.update_data(description=message.text)
     await message.answer("Введите стоимость товара")
     await state.set_state(AddProduct.price)
 
 
 @admin_router.message(AddProduct.description)
-async def add_name(message: types.Message, state= FSMContext):
+async def add_name(message: types.Message, state: FSMContext):
     await message.answer("Вы ввели недопустимые данные, введите текст описания товара")
 
 
 @admin_router.message(AddProduct.price, F.text)
-async def add_price(message: types.Message, state= FSMContext):
+async def add_price(message: types.Message, state: FSMContext):
     await state.update_data(price=message.text)
     await message.answer("Загрузите изображение товара")
     await state.set_state(AddProduct.image)
 
 
 @admin_router.message(AddProduct.price)
-async def add_name(message: types.Message, state= FSMContext):
+async def add_name(message: types.Message, state: FSMContext):
     await message.answer("Вы ввели недопустимые данные, введите текст стоимости товара")
 
 
 @admin_router.message(AddProduct.image, F.photo)
-async def add_image(message: types.Message, state= FSMContext):
+async def add_image(message: types.Message, state: FSMContext, session: AsyncSession):
+
     await state.update_data(image=message.photo[-1].file_id)
-    await message.answer("Товар добавлен", reply_markup=ADMIN_KB)
     data = await state.get_data()
-    await message.answer(str(data))
-    await state.clear()
+    try:
+        await orm_add_product(session, data)
+        await message.answer("Товар добавлен", reply_markup=ADMIN_KB)
+        await state.clear()
+    except Exception as e:
+        await message.answer(
+            f"Ошибка: \n{str(e)}\nОбратись к программеру", reply_markup=ADMIN_KB)
+        await state.clear()
 
 @admin_router.message(AddProduct.image)
 async def add_name(message: types.Message, state= FSMContext):
-    await message.answer("Вы ввели недопустимые данные, отправьте фото товара")
+    await message.answer("Отправьте фото товара")
